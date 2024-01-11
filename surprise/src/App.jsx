b89624131fdbe2dd180fffc3d43965db4824078f
shreyam <shreyam91183@gmail.com>
2024-01-11 11:44:39 +0530

    nnn

---
+++ surprise/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from './Homepage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ surprise/src/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+      <header>
+          <h2 className='text-center text-4xl font-italic'> Hello </h2>
+      </header>
+
+      <footer>
+        <h2 className='text-center text-black-500 opacity-1 relative'>Developed by Shrey</h2>
+      </footer>
+    </>
+  )
+}
+
+export default Homepage
